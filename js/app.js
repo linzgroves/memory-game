85eba416fc3a854e227284bf936c714a9db73e60
Lindsay Groves <lindsaythegroves@gmail.com>
2018-10-17 22:06:34 -0700

    A few JS updates for handling the clicking of cards

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
+const symbols = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
+  'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
+];
+
 /*
  * Create a list that holds all of your cards
  */
-const symbols = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
-  'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
-]
-
-
+let toggledCards = [];
 
 /*
  * Display the cards on the page
@@ -42,5 +42,29 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-const cards = document.querySelectorAll('.card');
-console.log(cards);
+/* Target parent element of .cards so addEventListener can be used */
+const deck = document.querySelector('.deck');
+
+/* Use addEventListener to listen for the cards being clicked and then call the toggle function */
+deck.addEventListener('click', event => {
+  const cardTarget = event.target;
+  if (cardTarget.classList.contains('card') && toggledCards.length < 2) {
+    toggleCard(cardTarget);
+    toggledCardsList(cardTarget);
+    if (toggledCards.length === 2) {
+      console.log("2 cards are toggled");
+    }
+  }
+});
+
+/* This toggles the open and show classes */
+function toggleCard(cardTarget) {
+  cardTarget.classList.toggle('open');
+  cardTarget.classList.toggle('show');
+}
+
+/* Function to push any toggled cards into the toggledCards array */
+function toggledCardsList(cardTarget) {
+  toggledCards.push(cardTarget);
+  console.log(toggledCards);
+}
