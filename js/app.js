416091d0868bc65fc1566c91b96bb327c84038b6
Lindsay Groves <lindsaythegroves@gmail.com>
2018-10-24 19:38:51 -0700

    Getting the shuffling to work with the existing <li> setup indiviudally in the HTML

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
-const symbols = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
-  'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
-];
-
 /*
  * Create a list that holds all of your cards
+ * This also targets the parent element of .card li items so addEventListener can be used
  */
-let toggledCards = [];
+const deck = document.querySelector('.deck');
 
 /*
  * Display the cards on the page
@@ -13,6 +10,17 @@ let toggledCards = [];
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+// Turn nodeList of all li elements into an array that can be used by shuffle function
+function shuffleCards() {
+  const cardsToShuffle = [].slice.call(document.querySelectorAll("li.card"));
+  const shuffledDeck = shuffle(cardsToShuffle);
+  for (card of shuffledDeck) {
+    deck.appendChild(card);
+  }
+}
+shuffleCards();
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -42,29 +50,54 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-/* Target parent element of .cards so addEventListener can be used */
-const deck = document.querySelector('.deck');
-
 /* Use addEventListener to listen for the cards being clicked and then call the toggle function */
 deck.addEventListener('click', event => {
   const cardTarget = event.target;
-  if (cardTarget.classList.contains('card') && toggledCards.length < 2) {
+  if (isValidClick(cardTarget)) {
     toggleCard(cardTarget);
     toggledCardsList(cardTarget);
     if (toggledCards.length === 2) {
-      console.log("2 cards are toggled");
+      checkIfMatch(cardTarget);
     }
   }
 });
 
+/* Check if click meets all the conditions */
+function isValidClick(cardTarget) {
+  return (
+    cardTarget.classList.contains('card') &&
+    !cardTarget.classList.contains('match') &&
+    toggledCards.length < 2 &&
+    !toggledCards.includes(cardTarget)
+  );
+}
+
 /* This toggles the open and show classes */
-function toggleCard(cardTarget) {
-  cardTarget.classList.toggle('open');
-  cardTarget.classList.toggle('show');
+function toggleCard(card) {
+  card.classList.toggle('open');
+  card.classList.toggle('show');
 }
 
 /* Function to push any toggled cards into the toggledCards array */
 function toggledCardsList(cardTarget) {
   toggledCards.push(cardTarget);
   console.log(toggledCards);
+}
+
+/* Check if the cards clicked and added to the toggledCards array match and add timeout so toggles isn't immediate*/
+function checkIfMatch() {
+  if (
+    toggledCards[0].firstElementChild.className ===
+    toggledCards[1].firstElementChild.className
+  ) {
+    toggledCards[0].classList.toggle('match');
+    toggledCards[1].classList.toggle('match');
+    toggledCards = [];
+  } else {
+    setTimeout(() => {
+      toggleCard(toggledCards[0]);
+      toggleCard(toggledCards[1]);
+      toggledCards = [];
+    }, 1000);
+  }
 }
